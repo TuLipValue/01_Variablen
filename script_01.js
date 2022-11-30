25ff115d9e074883ba4058cbc79d2b813dbcec51
TuLipValue <117446037+TuLipValue@users.noreply.github.com>
2022-11-30 20:57:44 +0100

    Var Deklaration und Werzuweisung + Ausgabemethode

--- script_01.js
+++ script_01.js
@@ -11,3 +11,13 @@
 // Konsolen-Ausgaben
 // console.log("Hallo");
 // console.log(firstnane);
+
+/*****    02 Deklaration + Wertzuweisung      ********** */
+
+let firstname;  // Deklaration (Definition)
+firstname = "Max"; // Wertzuweisung 
+console.log(firstname); // Ausgabe
+
+let familynName = "Mütze"; // Deklaration und Wertzuweisung
+console.log(familynName);  // Ausgabe
+console.log(firstname + " " + familynName); 
