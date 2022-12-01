a45fb6c5a418688b463fd07899a7f4a548fecead
TuLipValue <117446037+TuLipValue@users.noreply.github.com>
2022-12-01 01:01:48 +0100

    03a Konstanten

--- script_01.js
+++ script_01.js
@@ -31,10 +31,18 @@
 
 /* JS ist eine untypisierte Sprache ! untyped */
 
-let test;
-// test = "Hi";
-// test= 2;
-test = true;
+// let test;
+// // test = "Hi";
+// // test= 2;
+// test = true;
 
-console.log("Datentyp: " + typeof test);
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt:  " + test); // Ausgabe
+
+
+
+/*****    03a Konstanten  ********** */
+const test = "Hi"; // Variable mit KONSTANTEM Inhalt
+// test = "Hallo"; // KEINE neue Zuweisung  zur LZ möglich! --> Fehler
+
 console.log("Inhalt:  " + test); // Ausgabe
